e153cf98264558e9f17588ea61a7fee474957737
Takeshi YAEDA <t.yaeda@gmail.com>
2013-05-07 13:20:11 +0900

    Add emacs fullscreen setting on linux

---
+++ inits/linux-fullscreen.el
@@ -0,0 +1,9 @@
+(defun toggle-frame-fullscreen()
+  "Toggle full screen on X11"
+  (interactive)
+  (when (eq window-system 'x)
+    (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
+
+(global-set-key [(control return)] 'toggle-frame-fullscreen)
