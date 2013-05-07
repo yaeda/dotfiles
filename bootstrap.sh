8d181d69a1819603866c478ef5c8f5486d79b2ec
Takeshi YAEDA <t.yaeda@gmail.com>
2013-05-07 22:01:04 +0900

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,30 @@
 #
 
 # clone
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+[ ! -d $HOME/.zprezto ] && git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 # copy zsh configuration files
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-# chsh -s /bin/zsh
-
 # custom setting
-# echo "[ -f $HOME/.zshrc.mine ] && source $HOME/dotfile/.zshrc.mine" >> "${ZDOTDIR:-$HOME}/.zshrc"
+ln -fs $HOME/dotfiles/zsh/zpreztorc $HOME/.zpreztorc
+[ -f $HOME/.zshrc ] && echo "source $HOME/dotfiles/zsh/.zshrc.mine" >> "${ZDOTDIR:-$HOME}/.zshrc"
+
+
+#
+# Emacs
+#
+ln -fs $HOME/dotfiles/emacs/init.el $HOME/.emacs.d/init.el
+ln -fs $HOME/dotfiles/emacs/inits   $HOME/.emacs.d/inits
+
+
+#
+# other dotfiles
+#
+for dotfile in $HOME/dotfiles/.*
+do
+    ln -fs $dotfile $HOME/${dotfile:t}
+done
