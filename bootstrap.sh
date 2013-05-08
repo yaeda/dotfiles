354f212b3673214dbf0236dae5cc22461f6bbf96
Takeshi YAEDA <t.yaeda@gmail.com>
2013-05-08 09:48:07 +0900

    Improve install workflow

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,10 @@
 #
 
 # clone
-[ ! -d $HOME/.zprezto ] && git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+if [ ! -d $HOME/.zprezto ]; then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+    echo "[ -f $HOME/dotfiles/zsh/.zshrc.mine ] && source $HOME/dotfiles/zsh/.zshrc.mine" >> "${ZDOTDIR:-$HOME}/.zshrc"
+fi
 
 # copy zsh configuration files
 setopt EXTENDED_GLOB
@@ -13,20 +16,19 @@ done
 
 # custom setting
 ln -fs $HOME/dotfiles/zsh/zpreztorc $HOME/.zpreztorc
-[ -f $HOME/.zshrc ] && echo "source $HOME/dotfiles/zsh/.zshrc.mine" >> "${ZDOTDIR:-$HOME}/.zshrc"
 
 
 #
 # Emacs
 #
-ln -fs $HOME/dotfiles/emacs/init.el $HOME/.emacs.d/init.el
-ln -fs $HOME/dotfiles/emacs/inits   $HOME/.emacs.d/inits
+ln -fs  $HOME/dotfiles/emacs/init.el $HOME/.emacs.d/init.el
+ln -nfs $HOME/dotfiles/emacs/inits   $HOME/.emacs.d/inits
 
 
 #
 # other dotfiles
 #
-for dotfile in [.tmux.conf .giconfig]
+for dotfile in .tmux.conf .gitconfig
 do
     ln -fs $HOME/dotfiles/$dotfile $HOME/$dotfile
 done
