f87fb2ebf3301f1617efee2c5060c26267e2e7a6
Takeshi YAEDA <t.yaeda@gmail.com>
2017-08-25 22:54:05 +0900

    :bug: Remove 'set -U' from config.fish :fire:

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,5 @@
 . ~/.config/fish/aliases.fish
 
-# nodebrew
-set -U fish_user_paths $fish_user_paths $HOME/.nodebrew/current/bin
-
 # local config
 if test -e ~/.local_config.fish
     . ~/.local_config.fish
