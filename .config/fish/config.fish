19792cc3d92cc46dbce1922a332adfb731443c15
Takeshi Yaeda <t.yaeda@gmail.com>
2020-09-08 01:28:31 +0900

    refactor(fish): readable `fish_user_paths` setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,9 +13,26 @@ if type -q rbenv
 end
 set -x ANDROID_HOME $HOME/Library/Android/sdk
 set -x ANDROID_SDK_ROOT $HOME/Library/Android/sdk
-set fish_user_paths $HOME/.fastlane/bin $HOME/.nodebrew/current/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $HOME/Library/Python/2.7/bin /Applications/"Android Studio.app"/Contents/jre/jdk/Contents/Home/bin /usr/local/bin
 set -x JAVA_HOME /Applications/"Android Studio.app"/Contents/jre/jdk/Contents/Home
+
+# reset fish_user_paths
+set -g fish_user_paths
+
+set -g fish_user_paths /usr/local/bin $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/avr-gcc@7/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/lib/ruby/gems/2.7.0/bin" $fish_user_paths
+
+set -g fish_user_paths $HOME/.nodebrew/current/bin $fish_user_paths
+
+set -g fish_user_paths $HOME/Library/Python/2.7/bin $fish_user_paths
+
+set -g fish_user_paths $HOME/.fastlane/bin $fish_user_paths
+
+set -g fish_user_paths $JAVA_HOME/bin $fish_user_paths
+set -g fish_user_paths $ANDROID_HOME/platform-tools $fish_user_paths
+set -g fish_user_paths $ANDROID_HOME/emulator $fish_user_paths
+set -g fish_user_paths $ANDROID_HOME/tools $fish_user_paths
+set -g fish_user_paths $ANDROID_HOME/tools/bin $fish_user_paths
+
